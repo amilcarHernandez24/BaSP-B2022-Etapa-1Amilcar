35de6a475b0cfb611d69f2c7c086e7ce084a35c0
Amilcar Hernandez <amilcarjh24@gmail.com>
2022-09-16 08:58:43 -0300

    BaSP-W05-2022: new week creted and first block of JS done

---
+++ week-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,24 @@
+/**** Variables y Operadores ****/
+
+var firstNum = 4;
+var secNum = 3;
+var result = firstNum + secNum;
+console.log('Result: ' + result);
+
+
+var firstName = "My name is";
+var lastName = "Amilcar Hernandez";
+var fullName = firstName + ' ' + lastName;
+console.log('Name: ' + fullName);
+
+var textOne = "this is a text";
+var textTwo = "and this is another text";
+
+// var textOneSize = textOne.length;
+// console.log(textOneSize);
+
+// var textTwoSize = textTwo.length;
+// console.log(textTwoSize);
+
+var totalString = textOne.length + textTwo.length;
+console.log('String length: ' + totalString)
