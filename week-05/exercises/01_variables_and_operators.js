d4a60c179fe8e417c40a98f484fcc37e55ef38f6
Amilcar Hernandez <amilcarjh24@gmail.com>
2022-09-16 23:07:10 -0300

    BaSP-W05-2022: exercise 1 done

--- week-05/exercises/01_variables_and_operators.js
+++ week-05/exercises/01_variables_and_operators.js
@@ -1,24 +1,33 @@
-/**** Variables y Operadores ****/
+console.log('--EXERCISE 1: Variables and Operators');
+
+/* a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en 
+una 3er variable.*/
+
+console.log('--EXERCISE 1.a:');
 
 var firstNum = 4;
 var secNum = 3;
 var result = firstNum + secNum;
+
 console.log('Result: ' + result);
 
+/* b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable. */
 
-var firstName = "My name is";
-var lastName = "Amilcar Hernandez";
+console.log('--EXERCISE 1.b:');
+
+var firstName = 'My name is';
+var lastName = 'Amilcar Hernandez';
 var fullName = firstName + ' ' + lastName;
+
 console.log('Name: ' + fullName);
 
-var textOne = "this is a text";
-var textTwo = "and this is another text";
+/* c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando 
+el resultado de la suma en una 3er variable (utilizar length). */
 
-// var textOneSize = textOne.length;
-// console.log(textOneSize);
+console.log('--EXERCISE 1.c:');
 
-// var textTwoSize = textTwo.length;
-// console.log(textTwoSize);
-
+var textOne = 'this is a text';
+var textTwo = 'and this is another text';
 var totalString = textOne.length + textTwo.length;
-console.log('String length: ' + totalString)
+
+console.log('String length: ' + totalString);
---
+++ week-05/exercises/02_strings.js
@@ -0,0 +1,34 @@
+console.log('--EXERCISE 2: Strings');
+
+/* a. Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula 
+(utilizar toUpperCase).*/
+
+console.log('--EXERCISE 2.a:');
+
+/* b. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 
+caracteres guardando el resultado en una nueva variable (utilizar substring).*/
+
+console.log('--EXERCISE 2.b:');
+
+/* c. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3 caracteres
+guardando el resultado en una nueva variable (utilizar substring).*/
+
+console.log('--EXERCISE 2.c:');
+
+/* d. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en 
+mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, 
+toLowerCase y el operador +).*/
+
+console.log('--EXERCISE 2.d:');
+
+/* e. Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición 
+del primer espacio en blanco y guardarla en una variable (utilizar indexOf). */
+
+console.log('--EXERCISE 2.e:');
+
+/* f. Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). 
+Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de ambas
+palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el 
+operador +). */
+
+console.log('--EXERCISE 2.f:');
