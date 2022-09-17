7c8153f2d7ee7444656b759a5d31aeeba69e1f68
Amilcar Hernandez <amilcarjh24@gmail.com>
2022-09-17 12:55:41 -0300

    BaSP-W05-2022: arrays finished

--- week-05/exercises/02_strings.js
+++ week-05/exercises/02_strings.js
@@ -5,15 +5,29 @@ console.log('--EXERCISE 2: Strings');
 
 console.log('--EXERCISE 2.a:');
 
+var upperText = 'Esta es mi variable';
+
+console.log('Resultado: ' + upperText.toUpperCase());
+
 /* b. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 
 caracteres guardando el resultado en una nueva variable (utilizar substring).*/
 
 console.log('--EXERCISE 2.b:');
 
+var subText = 'Segundo ejercicio';
+var newSubText = subText.substring(0,5);
+
+console.log('Resultado: ' + newSubText);
+
 /* c. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3 caracteres
 guardando el resultado en una nueva variable (utilizar substring).*/
 
 console.log('--EXERCISE 2.c:');
+
+var subTextBack = 'Tercer ejercicio';
+var newSubTextBack = subTextBack.substring(16, 13);
+
+console.log('Resultado: ' + newSubTextBack);
 
 /* d. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en 
 mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, 
@@ -21,10 +35,18 @@ toLowerCase y el operador +).*/
 
 console.log('--EXERCISE 2.d:');
 
+var firstInUpper = 'se esta complicando';
+
+
 /* e. Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición 
 del primer espacio en blanco y guardarla en una variable (utilizar indexOf). */
 
 console.log('--EXERCISE 2.e:');
+
+var findSpace = 'Esta es un variable';
+var spaceFound = findSpace.indexOf(' ');
+
+console.log('Resultado: ' + spaceFound);
 
 /* f. Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). 
 Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de ambas
@@ -32,3 +54,5 @@ palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, subs
 operador +). */
 
 console.log('--EXERCISE 2.f:');
+
+var twoLongWords = 'Palabrotas Largotototas';
--- week-05/exercises/03_arrays.js
+++ week-05/exercises/03_arrays.js
@@ -1,1 +1,75 @@
-/**** Arrays ****/
+console.log('--EXERCISE 3: Arrays');
+
+
+/* a. Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
+"Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log).*/
+
+console.log('--EXERCISE 3.a:');
+
+var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre',
+'Octubre', 'Noviembre', 'Diciembre'];
+
+console.log('Result: ' + months[4], months[10]);
+
+// b. Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).
+
+console.log('--EXERCISE 3.b:');
+
+var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre',
+'Octubre', 'Noviembre', 'Diciembre'];
+
+console.log('Result: ' + months.sort());
+
+// c. Agregar un elemento al principio y al final del array (utilizar unshift y push).
+
+console.log('--EXERCISE 3.c:');
+
+var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre',
+'Octubre', 'Noviembre', 'Diciembre'];
+
+months.unshift('Principio');
+months.push('Final');
+
+console.log('Result: ' + months);
+
+// d. Quitar un elemento del principio y del final del array (utilizar shift y pop).
+
+console.log('--EXERCISE 3.d:');
+
+var months = ['Principio', 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre',
+'Octubre', 'Noviembre', 'Diciembre', 'Final'];
+
+months.shift();
+months.pop();
+
+console.log('Result: ' + months);
+
+// e. Invertir el orden del array (utilizar reverse).
+
+console.log('--EXERCISE 3.e:');
+
+var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre',
+'Octubre', 'Noviembre', 'Diciembre'];
+
+console.log('Result: ' + months.reverse());
+
+// f. Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).
+
+console.log('--EXERCISE 3.f:');
+
+var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre',
+'Octubre', 'Noviembre', 'Diciembre'];
+var monthsTogether = months.join('-');
+
+console.log('Result: ' + monthsTogether);
+
+// g. Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).
+
+console.log('--EXERCISE 3.g:');
+
+var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre',
+'Octubre', 'Noviembre', 'Diciembre'];
+
+var partOfYear = months.slice(4,11);
+
+console.log('Result: ' + partOfYear);
